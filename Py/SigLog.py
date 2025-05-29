7b85e0de094abb049cd12f1fc420d4dc340c3e6b
Victor <victordamotapereira@outlook.com>
2025-05-29 15:58:31 -0300

    feat

---
+++ Py/SigLog.py
@@ -0,0 +1,36 @@
+import pyautogui 
+import time
+import keyboard 
+
+
+pyautogui.press('win')
+time.sleep(3)
+pyautogui.write('Google',interval=0.2)      
+time.sleep(2)
+pyautogui.press('enter')        
+
+#espera o app abrir
+time.sleep(9)
+pyautogui.press('tab')
+pyautogui.press('enter')
+
+pyautogui.write("https://sig.ifc.edu.br/sigaa/mobile/touch/login.jsf,interval", interval = 0.2)
+pyautogui.press('enter')
+
+time.sleep(4)
+pyautogui.press('enter')
+
+time.sleep(5)
+
+for i in range (2):
+    pyautogui.press('tab', interval=0.1)
+    
+pyautogui.write('', interval=0.1)
+time.sleep(1)
+pyautogui.press('tab')
+
+pyautogui.write('', interval=0.1)
+time.sleep(1)   
+pyautogui.press('enter')
+
+time.sleep(3)
